1c520c4f7d05d8e114d6ce91d61de06459eb9395
snamiki1212 <snamiki1212@gmail.com>
2024-11-16 15:52:49 +0900

    fix: update

--- src/cmd/pr_review_rate/search.zsh
+++ src/cmd/pr_review_rate/search.zsh
@@ -8,77 +8,74 @@
 
 REPO="SSK-TBD/bluage" # プロジェクトのリポジトリ名（例: openai/chatgpt）
 LABEL="cc_backend" # タグBを設定
-LIMIT="30" # 最大数1_000 しか gh が対応できないので、これ以上欲しい場合はページネーション or gh 以外で考える
+LIMIT="1000" # 最大数1_000 しか gh が対応できないので、これ以上欲しい場合はページネーション or gh 以外で考える
 ME="snamiki1212"
-SEARCH_FROM="2024-11-01"
-SEARCH_TO="2024-12-01"
+SEARCH_FROM="2024-10-01"
+SEARCH_TO="2024-11-01"
 verbose=true
 
-######################################################
+##########q############################################
 ##
 ## Main
 ##
 ######################################################
-cmd="gh search prs --repo $REPO --label $LABEL --reviewed-by $ME --created \"$SEARCH_FROM..$SEARCH_TO\" --limit $LIMIT --json number,labels,reviews"
+cmd_all_prs="gh search prs --repo $REPO --label $LABEL --created \"$SEARCH_FROM..$SEARCH_TO\" --limit $LIMIT --json id"
+cmd_my_prs="$cmd_all_prs --reviewed-by $ME"
 
 # log
-echo "Runnin following cmd...
-> $cmd
+echo "Running cmd_all_prs on following...
+> $cmd_all_prs
 "
 
-# PR情報を取得
-PR_DATA=$(eval $cmd) # cmd 実行
-PR_DATA=$(echo $PR_DATA | tr -d '[:cntrl:]') # 改行コードを削除: https://qiita.com/re-sasaki/items/7d34109cf209a8ac753c
+# PRs 取得
+ALL_PRs=$(eval $cmd_all_prs) # cmd 実行
+ALL_PRs=$(echo $ALL_PRs | tr -d '[:cntrl:]') # 改行コードを削除: https://qiita.com/re-sasaki/items/7d34109cf209a8ac753c
+
+# log
+echo "Running cmd_my_prs on following...
+> $cmd_my_prs
+"
+
+# PRs 取得
+MY_PRs=$(eval $cmd_my_prs) # cmd 実行
+MY_PRs=$(echo $MY_PRs | tr -d '[:cntrl:]') # 改行コードを削除: https://qiita.com/re-sasaki/items/7d34109cf209a8ac753c
 
 # echo "<<"
-# # echo "PR_DATA: $PR_DATA"
-# x=$(echo $PR_DATA | jq .)
+# # echo "MY_PRs: $MY_PRs"
+# x=$(echo $MY_PRs | jq .)
 # echo $x
 # echo "||"
 
 # 総PR数を取得
-TOTAL=$(echo "$PR_DATA" | jq '. | length')
+TOTAL=$(echo "$ALL_PRs" | jq '. | length')
 if [ "$TOTAL" -eq 0 ]; then
   echo "タグ \"$LABEL\" が付いたPRはありません。"
   exit 0
 fi
 
-# PR取得
-APPROVED_PRs=$(echo "$PR_DATA" | jq --arg ME "$ME" '[.[] | select(.reviews[]?.author.login == $ME)]') # PRへApprove系した数（approve/comment/block）
-COMMENTED_PRs=$(echo "$PR_DATA" | jq --arg ME "$ME" '[.[] | select(.comments[]?.author.login == $ME)]') # PRへコメントした数
-
-
-# echo "APPROVED_PRs: $APPROVED_PRs"
 if [ "$verbose" = true ]; then
-  debug=$(echo $APPROVED_PRs | jq '[.[] | .number]')
-  echo "approved_pr_numbers: $debug"
+  ids=$(echo $MY_PRs | jq '[.[] | .id]')
+  echo "ids: $ids"
 fi
 
-
 # 件数取得
-APPROVED_COUNT=$(echo $APPROVED_PRs | jq '. | length')
-COMMENTED_COUNT=$(echo $COMMENTED_PRs | jq '. | length')
+APPROVED_COUNT=$(echo $MY_PRs | jq '. | length')
 
 # 比率を計算
 APPROVAL_RATE=$(echo "scale=2; ($APPROVED_COUNT / $TOTAL) * 100" | bc)
-COMMENT_RATE=$(echo "scale=2; ($COMMENTED_COUNT / $TOTAL) * 100" | bc)
 
 # Output
 result=$(jq -n \
   --arg from "$SEARCH_FROM" \
   --arg to "$SEARCH_TO" \
   --arg approved_count "$APPROVED_COUNT" \
-  --arg commented_count "$COMMENTED_COUNT" \
   --arg approval_rate "$APPROVAL_RATE" \
-  --arg comment_rate "$COMMENT_RATE" \
   --arg total "$TOTAL" \
   '{
     from: $from,
     to: $to,
     approved_count: $approved_count,
-    commented_count: $commented_count,
     approval_rate: $approval_rate,
-    comment_rate: $comment_rate,
     total: $total
   }')
 echo $result
